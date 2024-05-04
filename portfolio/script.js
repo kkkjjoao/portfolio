44cc770cf0f46184a3eade29c111d24e85ce0c1a
João Victor <154566658+kkkjjoao@users.noreply.github.com>
2024-05-03 23:22:16 -0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,35 @@
+function mostrarImagem() {
+  setTimeout(function(){
+    document.getElementById("ihtml").style.display = "block";
+  },200);
+}
+
+function esconderImagem() {
+  setTimeout(function(){
+    document.getElementById("ihtml").style.display = "none";
+  },200); 
+}
+
+function mostrarImagem2() {
+  setTimeout(function(){
+    document.getElementById("icss").style.display = "block";
+  },200);
+}
+
+function esconderImagem2() {
+  setTimeout(function(){
+    document.getElementById("icss").style.display = "none";
+  },200); 
+}
+
+function mostrarImagem3() {
+  setTimeout(function(){
+    document.getElementById("ijs").style.display = "block";
+  },200);
+}
+
+function esconderImagem3() {
+  setTimeout(function(){
+    document.getElementById("ijs").style.display = "none";
+  },200); 
+}
